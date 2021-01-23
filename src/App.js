ada87ad21cb37ecd14a1d179e04878d0dd9162c1
alsgur6180 <58066921+alsgur6180@users.noreply.github.com>
2021-01-24 07:23:49 +0900

    get movie API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import axios from "axios";
 
-function Food(favorite) {
-  return <h1>I like {favorite}</h1>;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Food />
-    </div>
-  );
-}
-
 export default App;
